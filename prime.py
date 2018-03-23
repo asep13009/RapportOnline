743b9ca53e82426fc8edce5c8eb26524759f69c0
sucif <sucifitriyani508@gmail.com>
2018-03-23 15:19:32 +0700

    suci

---
+++ prime.py
@@ -0,0 +1,14 @@
+def prime(bilangan):
+    count = 0
+    for i in range(1,bilangan+1):
+        if(bilangan %i==0):
+            count+=1
+    if (count==2):
+        return "prime"
+    else:
+        return "not prime"
+tes= prime(5)
+print(tes)
+
+
+
